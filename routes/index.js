c8f219a1992270c96264c2b516b847c9bcb25088
jimthedev <jimthedev@gmail.com>
2020-10-20 06:19:15 -0500

    fix: db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+const { Pool } = require('pg');
 var router = express.Router();
 
+const pool = new Pool({
+  connectionString: process.env.QOVERY_DATABASE_MY_DB_CONNECTION_URI,
+})
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+router.get('/', async function (req, res, next) {
+    // you can also use async/await
+    const res = await pool.query('SELECT NOW()')
+    
+    res.render('index', {title: 'Express' + res});
+    await pool.end()
 });
+
+
 
 module.exports = router;
